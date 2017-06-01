18a27362d83eb99ecf05ba18174c388c2482f645
Acaki <axsz4298@gmail.com>
2017-06-01 15:18:12 +0800

    Add scrollable background

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ var game = new Phaser.Game(600, 800, Phaser.AUTO, '', {preload: preload, create:
 
 function preload() {
   game.load.image('player', 'assets/player.png');
+  game.load.image('background', 'assets/starBackground.png');
 }
 
+var background;
 var player;
 var cursors;
 function create() {
+  background = game.add.tileSprite(0, 0, game.world.width, game.world.height, 'background');
+  background.autoScroll(0, -30);
+
   game.physics.startSystem(Phaser.Physics.ARCADE);
   player = game.add.sprite(game.world.width / 2 - 50, game.world.height - 76, 'player');
   game.physics.arcade.enable(player);
@@ -19,15 +24,15 @@ function update() {
   player.body.velocity.y = 0;
 
   if (cursors.left.isDown) {
-    player.body.velocity.x = -200;
+    player.body.velocity.x = -300;
   }
   else if (cursors.right.isDown) {
-    player.body.velocity.x = 200;
+    player.body.velocity.x = 300;
   }
   if (cursors.up.isDown) {
-    player.body.velocity.y = -200;
+    player.body.velocity.y = -300;
   }
   else if (cursors.down.isDown) {
-    player.body.velocity.y = 200;
+    player.body.velocity.y = 300;
   }
 }
