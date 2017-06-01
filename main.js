777f2d8e05f7ab96bc925eba63bf4fe44987153d
Acaki <axsz4298@gmail.com>
2017-06-01 14:29:52 +0800

    Add player plane

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+var game = new Phaser.Game(600, 800, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
 function preload() {
-
+  game.load.image('player', 'assets/player.png');
 }
 
+var player;
 function create() {
-  
+  player = game.add.sprite(game.world.width / 2 - 50, game.world.height - 76, 'player');
+}
+
+function update() {
+
 }
