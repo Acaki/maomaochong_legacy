49adc111cd02ea1d1d22281f172fda0819876970
Acaki <axsz4298@gmail.com>
2017-06-01 14:52:30 +0800

    Allow player to move up/down/right/left

--- main.js
+++ main.js
@@ -5,10 +5,29 @@ function preload() {
 }
 
 var player;
+var cursors;
 function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE);
   player = game.add.sprite(game.world.width / 2 - 50, game.world.height - 76, 'player');
+  game.physics.arcade.enable(player);
+  player.body.collideWorldBounds = true;
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
+  player.body.velocity.x = 0;
+  player.body.velocity.y = 0;
 
+  if (cursors.left.isDown) {
+    player.body.velocity.x = -200;
+  }
+  else if (cursors.right.isDown) {
+    player.body.velocity.x = 200;
+  }
+  if (cursors.up.isDown) {
+    player.body.velocity.y = -200;
+  }
+  else if (cursors.down.isDown) {
+    player.body.velocity.y = 200;
+  }
 }
