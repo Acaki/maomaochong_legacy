3ac67367672066d67027de07c2c1f140a62afc4c
RBRBRB <qaz8899921@gmail.com>
2017-06-02 21:00:41 +0800

    Update enemyType.js

--- js/enemyType.js
+++ js/enemyType.js
@@ -2,11 +2,11 @@ var EnemyType = {};
 
 EnemyType.Trash = function (game) {
   Phaser.Group.call(this, game, game.world, 'Trash Enemy', false, true, Phaser.Physics.ARCADE);
-
-  this.enemySpeed = 200;
+    
   //Add 10 trash enemies into this group
   for (var i = 0; i < 10; i++) {
     this.add(new Enemy(game, 'enemy3'), true);
+    this.add(new Enemy(game, 'enemy4'), true);
   }
   return this;
 }
@@ -22,6 +22,8 @@ EnemyType.Trash.prototype.launch = function() {
   var halfWidth = enemy.width / 2;
   var x = this.game.rnd.integerInRange(0 + halfWidth, this.game.width - halfWidth);
   var angle = this.game.rnd.integerInRange(45, 135);
+  var speed = game.rnd.between(120,200);
+ 
   //Launch the enemy starting on top of the screen
-  enemy.launch(x, 0, angle, this.enemySpeed, 0);
+  enemy.launch(x, 0, angle, speed, 0);
 }
