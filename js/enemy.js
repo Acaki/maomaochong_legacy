3c1441bf631bab081eaa1053d548ceec5e396692
RBRBRB <qaz8899921@gmail.com>
2017-06-03 19:57:25 +0800

    Update enemy.js

--- js/enemy.js
+++ js/enemy.js
@@ -2,13 +2,15 @@ var Enemy = function(game, key ,enemyHP) {
   //Call constructor of Phaser.Sprite to initialize this
   Phaser.Sprite.call(this, game, 0, 0, key);
 
+  //Set the origin point of the sprite to its center
+  this.anchor.set(0.5);
   this.checkWorldBounds = true;
   this.outOfBoundsKill = true;
   //Record whether sprite is processed by the update() function
   this.exists = false;
   this.Hp = enemyHP;
-}
 
+}
 //Enemy inherited from Phaser.sprite
 Enemy.prototype = Object.create(Phaser.Sprite.prototype);
 Enemy.prototype.constructor = Enemy;
@@ -24,5 +26,19 @@ Enemy.prototype.launch = function(x, y, angle, speed, xAccel) {
   //Set Enemy's velocity that is calculated from the given angle and speed
   this.game.physics.arcade.velocityFromAngle(angle, speed, this.body.velocity);
   this.body.acceleration.x = xAccel;
+}
+
+Enemy.prototype.isDead = function(bullet, enemy)
+{
+  bullet.kill();
+  enemy.Hp -= bullet.damage;
+  if(enemy.Hp <= 0)
+  {
+    enemy.Hp = game.rnd.between(2,3);
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
