af34193feac5ffa1702a0be57dafbc29706174f4
RBRBRB <qaz8899921@gmail.com>
2017-06-03 14:38:28 +0800

    Update enemy.js

--- js/enemy.js
+++ js/enemy.js
@@ -1,14 +1,12 @@
-var Enemy = function(game, key , enemyHp) {
+var Enemy = function(game, key ,enemyHP) {
   //Call constructor of Phaser.Sprite to initialize this
-  Phaser.Sprite.call(this, game, 0, 0, key );
-  //Set the origin point of the sprite to its center
-  this.anchor.set(0.5);
+  Phaser.Sprite.call(this, game, 0, 0, key);
+
   this.checkWorldBounds = true;
   this.outOfBoundsKill = true;
   //Record whether sprite is processed by the update() function
   this.exists = false;
-  this.Hp = enemyHp;
-
+  this.Hp = enemyHP;
 }
 
 //Enemy inherited from Phaser.sprite
@@ -27,3 +25,4 @@ Enemy.prototype.launch = function(x, y, angle, speed, xAccel) {
   this.game.physics.arcade.velocityFromAngle(angle, speed, this.body.velocity);
   this.body.acceleration.x = xAccel;
 }
+
