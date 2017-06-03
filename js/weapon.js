67aa338f4989944816b4cdcf3a0b391a4bae0530
RBRBRB <qaz8899921@gmail.com>
2017-06-03 23:57:44 +0800

    Update weapon.js

--- js/weapon.js
+++ js/weapon.js
@@ -53,3 +53,34 @@ Weapon.SingleBullet.prototype.fire = function (source) {
 
   this.nextFire = this.game.time.time + this.fireRate;
 }
+
+//Enemy weapons
+var Weapon2 = {};
+Weapon2.EnemyBullet = function(game){
+  Phaser.Group.call(this, game, game.world, 'EnemyBullet', false, true, Phaser.Physics.ARCADE);
+
+  //this.powerLevel = 0;
+  this.nextFire = 0;
+  this.bulletSpeed = 300;
+  this.fireRate = 70;
+  for (var i = 0; i < 128; i++) {
+    this.add(new Bullet(game, 'bullet1', 1), true);
+  }
+  return this;
+}
+Weapon2.EnemyBullet.prototype = Object.create(Phaser.Group.prototype);
+Weapon2.EnemyBullet.prototype.constructor = Weapon2.EnemyBullet;
+
+Weapon2.EnemyBullet.prototype.fire = function (source) {
+  if (this.game.time.time < this.nextFire) {
+    return;
+  }
+
+  var x1, y1;
+    x1 = source.x ;
+    y1 = source.y ;
+    this.getFirstExists(false).fire(x1, y1, 90, this.bulletSpeed, 0);
+
+  this.nextFire = this.game.time.time + this.fireRate;
+}
+
