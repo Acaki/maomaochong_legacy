4c6563762fc87d266f202016b0ee9bc3423cea11
RBRBRB <qaz8899921@gmail.com>
2017-06-04 15:20:15 +0800

    Add enemy level

--- js/enemyType.js
+++ js/enemyType.js
@@ -4,8 +4,11 @@ EnemyType.Trash = function (game) {
   Phaser.Group.call(this, game, game.world, 'Trash Enemy', false, true, Phaser.Physics.ARCADE);
 
   //Add 10 trash enemies into this group
-  for (var i = 0; i < 10; i++) {
-    this.add(new Enemy(game, 'enemy3' , 3), true);
+  for (var i = 0; i < 20; i++) {
+    var newEnemy = new Enemy(game, 'enemy3', 5 , 1);
+    this.add(newEnemy, true);
+    //Reset the enemy's health when it's killed
+    newEnemy.events.onKilled.add(newEnemy.resetHealth, newEnemy);
   }
   return this;
 }
@@ -31,8 +34,10 @@ EnemyType.Trash2 = function (game) {
   Phaser.Group.call(this, game, game.world, 'Trash Enemy2', false, true, Phaser.Physics.ARCADE);
 
   //Add 10 trash enemies into this group
-  for (var i = 0; i < 10; i++) {
-    this.add(new Enemy(game, 'enemy4' , 2), true);
+  for (var i = 0; i < 20; i++) {
+    var newEnemy = new Enemy(game, 'enemy4', 2 , 2);
+    this.add(newEnemy, true);
+    newEnemy.events.onKilled.add(newEnemy.resetHealth, newEnemy);
   }
   return this;
 }
@@ -44,11 +49,12 @@ EnemyType.Trash2.prototype.launch = function() {
   var enemy = this.getFirstExists(false);
   enemy.body.drag.x = 100;
   //Prevent sprite being cut off on the edges
-  var halfWidth = enemy.width / 2;
+  var halfWidth = enemy.body.halfWidth;
   var x = this.game.rnd.integerInRange(0 + halfWidth, this.game.width - halfWidth);
   var angle = this.game.rnd.integerInRange(45, 135);
-  var speed = game.rnd.between(120,200);
+  var speed = game.rnd.between(200,250);
 
   //Launch the enemy starting on top of the screen
   enemy.launch(x, 0, angle, speed, 0);
 }
+
