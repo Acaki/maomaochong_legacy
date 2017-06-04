d4756127bf02dc6ae68548b17cce4191c6afa8a7
RBRBRB <qaz8899921@gmail.com>
2017-06-04 15:17:45 +0800

    Add new enemy weapon

--- js/weapon.js
+++ js/weapon.js
@@ -54,12 +54,11 @@ Weapon.SingleBullet.prototype.fire = function (source) {
   this.nextFire = this.game.time.time + this.fireRate;
 }
 
-//Enemy weapons
+//Enemy weapons1
 var Weapon2 = {};
 Weapon2.EnemyBullet = function(game){
-  Phaser.Group.call(this, game, game.world, 'EnemyBullet', false, true, Phaser.Physics.ARCADE);
+  Phaser.Group.call(this, game, game.world, 'EnemyBullet1', false, true, Phaser.Physics.ARCADE);
 
-  //this.powerLevel = 0;
   this.nextFire = 0;
   this.bulletSpeed = 300;
   this.fireRate = 70;
@@ -75,11 +74,40 @@ Weapon2.EnemyBullet.prototype.fire = function (source) {
   if (this.game.time.time < this.nextFire) {
     return;
   }
+  var x, y;
+    x = source.x ;
+    y = source.y ;
+    this.getFirstExists(false).fire(x, y, 90, this.bulletSpeed, 0);
 
-  var x1, y1;
-    x1 = source.x ;
-    y1 = source.y ;
-    this.getFirstExists(false).fire(x1, y1, 90, this.bulletSpeed, 0);
+  this.nextFire = this.game.time.time + this.fireRate;
+}
+
+//Enemy weapons2
+var Weapon3 = {};
+Weapon3.EnemyBullet = function(game){
+  Phaser.Group.call(this, game, game.world, 'EnemyBullet2', false, true, Phaser.Physics.ARCADE);
+
+  this.nextFire = 0;
+  this.bulletSpeed = 500;
+  this.fireRate = 70;
+  for (var i = 0; i < 128; i++) {
+    this.add(new Bullet(game, 'bullet2', 1), true);
+  }
+  return this;
+}
+Weapon3.EnemyBullet.prototype = Object.create(Phaser.Group.prototype);
+Weapon3.EnemyBullet.prototype.constructor = Weapon3.EnemyBullet;
+
+Weapon3.EnemyBullet.prototype.fire = function (source) {
+  if (this.game.time.time < this.nextFire) {
+    return;
+  }
+  var x, y;
+    x = source.x ;
+    y = source.y ;
+    this.getFirstExists(false).fire(x, y, 90, this.bulletSpeed, 0);
+    x += 30;
+    this.getFirstExists(false).fire(x, y, 90, this.bulletSpeed, 0);
 
   this.nextFire = this.game.time.time + this.fireRate;
 }
